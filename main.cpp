5f8e5e9e9b996ac1f15a6868e33a874b26c74fcc
nguyenngodinh <nguyenngodinh@outlook.com>
2018-06-25 01:08:43 +0700

    properly run, not optimize

--- main.cpp
+++ main.cpp
@@ -24,26 +24,23 @@
 #include <unistd.h>
 #include <set>
 #include <functional>
-
+#include <math.h>
 using namespace std;
 
+struct lessKMerge{
+    bool operator()(pair<ifstream*,string> lhs, pair<ifstream*,string> rhs){
+        return  lhs.second.compare(rhs.second)<0;
+    }
+};
 
-void print_arr(int A[], int n)
-{
-    int i;
-    for (i = 0; i < n; i++)
-        printf("%d\n", A[i]);
-}
+struct lessString{
+    bool operator()(string lhs, string rhs){
 
-int compar(const void *p1, const void *p2)
-{
-    int *i1, *i2;
 
-    i1 = (int *)p1;
-    i2 = (int *)p2;
-
-    return *i1 - *i2;
-}
+        return lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+//        return lhs.compare(rhs)<0;
+    }
+};
 
 char *form_filename(int chunk, char *dir)
 {
@@ -75,34 +72,15 @@ int save_buf(const vector<string>& data, char *dir, int chunk)
 {
     char *path;
     path = form_filename(chunk, dir);
-    cout << "Save chunk " << chunk << " word size=" << data.size() << " to " << path << endl;
-    ofstream of(path);
+    cerr << "Save chunk " << chunk << " size=" << data.size()/pow(pow(2,10),3) << "GB to " << path << endl;
+    cerr << "-----------------------------------------------\n";
+    ofstream of(path, ofstream::binary);
     int data_size = data.size();
-    for(int i=0; i<(data_size-1); i++)
-    {
-        of << data[i];
-        of << " ";
-    }
+    copy(data.begin(), data.begin()+data_size-1, ostream_iterator<string>(of, " "));
     of << data[data_size-1];
     of.close();
 
-
-
-//    f = fopen(path, "w");
-//    if (f == NULL) {
-//        perror("fopen");
-//        ret = -1;
-//        goto exit;
-//    }
-
-//    nwrite = fwrite(buf, sizeof(int), n, f);
-//    if (nwrite != n) {
-//        perror("fwrite");
-//        ret = -1;
-//        goto exit;
-//    }
-
-exit:
+//exit:
     if (path) {
         free(path);
     }
@@ -110,32 +88,32 @@ exit:
 }
 
 // Return chunks number of -1 on error
-int split(ifstream *f, off_t filesize, char *dir, char *buf, size_t bufsize)
+int split(ifstream *f, off_t filesize, char *dir, size_t &max_word_length, size_t bufsize)
 {
-    // XXX: we assume that filesize % bufsize == 0
-    int chunks = filesize / bufsize;
-//    cout << "file size = " << filesize << endl;
-//    cout << "buffer size = " << bufsize << endl;
-//    cout << "Chunks = " << chunks << endl;
     int size_of_text = 0;
     vector<string> data;
     string word;
-//    for (i = 0; i < chunks; i++)
     int i = 0;
     while(!f->eof())
     {
         *f >> word;
-//        std::cout << word << endl;
+        if(word.empty())
+            cerr << "+";
+        if(word.size() > max_word_length)
+            max_word_length = word.size();
+
         if(size_of_text+word.size() < bufsize){
             data.push_back(word);
             size_of_text++;
             size_of_text+= word.size();
         }else{
-//            cout << "data[" << i << "].size=" << data.size()  << "--> size of text = " << size_of_text<< endl;
-            sort(data.begin(), data.end());
-//            cout << "sorted: ";
-//            copy(data.begin(), data.end(), ostream_iterator<string>(cout, " "));
-//            cout << endl << "-------------------------------" << endl;
+            cerr << i << ":";
+            cerr << "sorting data size=" << data.size() << "~" << size_of_text/pow(pow(2,10),3) << "GB" << endl;
+            clock_t start_temp = clock();
+            std::make_heap(data.begin(), data.end());
+            std::sort_heap(data.begin(), data.end());
+            clock_t end_temp = clock();
+            cerr << "complete sort! in " << (end_temp-start_temp)/CLOCKS_PER_SEC << "secs" << endl;
             save_buf(data, dir, i);
 
             i++;
@@ -144,131 +122,95 @@ int split(ifstream *f, off_t filesize, char *dir, char *buf, size_t bufsize)
             size_of_text = word.size();
         }
     }
-    return chunks;
-}
+    if(!data.empty()){
+        cerr << i << ":";
+        cerr << "sorting data size=" << data.size() << "~" << size_of_text/pow(pow(2,10),3) << "GB" << endl;
+        clock_t start_temp = clock();
+        std::make_heap(data.begin(), data.end());
+        std::sort_heap(data.begin(), data.end());
+        clock_t end_temp = clock();
+        cerr << "complete sort! in " << (end_temp-start_temp)/CLOCKS_PER_SEC << "secs" << endl;
+        save_buf(data, dir, i);
 
-int merge(char *dir/*, char *buf*//*, size_t bufsize*/, int chunks, size_t offset)
-{
-    FILE *f;
-    int slice;
-    char *path;
-    int buf_offset;
-    int i, n;
-
-//    slice = bufsize / chunks;
-    buf_offset = 0;
-
-    // Each chunk has `chunk` number of slices.
-    for (i = 0; i < chunks; i++)
-    {
-        path = form_filename(i, dir);
-        fprintf(stderr, "Chunk %s at offset %zu\n", path, offset);
-
-        f = fopen(path, "rb");
-        if (f == NULL) {
-            perror("fopen");
-            return -1;
-        }
-        fseek(f, offset, SEEK_SET);
-
-
-        // Accumulate slices from each chunk in buffer.
-//        n = fread(buf + buf_offset, 1, slice, f);
-        buf_offset += n;
-        fclose(f);
+        i++;
+        data.clear();
     }
-
-    // Thoughout this function we used buf as char array to use byte-addressing.
-    // But now we need to act on ints inside that buffer, so we cast it to actual type.
-    n = buf_offset / sizeof(int);
-//    qsort(buf, n, sizeof(int), compar);
-//    print_arr((int *)buf, n);
-
-    return 0;
+    f->close();
+    delete f;
+    cerr << "Complete split " << filesize/pow(pow(2,10),3) << "GB file to " << i << " chunk file of size " << bufsize/pow(10,9) << "GB" << endl;
+    return i;
 }
-struct lessKMerge{
-    bool operator()(pair<ifstream*,string> lhs, pair<ifstream*,string> rhs){
-        return  lhs.second.compare(rhs.second)<0;
-    }
-};
-
 
 int external_merge_sort(ifstream *f, off_t filesize, char *dir, size_t bufsize)
 {
     int chunks;
     char *buf;
-    size_t chunk_offset;
+    size_t max_word_length = 0;
+    size_t max_store_elem = 0;
 
-    // XXX: Here is the only buffer available to us.
     buf = new char[bufsize];
     if (!buf) {
         perror("new");
         return -ENOMEM;
     }
-
     // Phase 1: split file to sorted chunks of size bufsize.
-    chunks = split(f, filesize, dir, buf, bufsize);
-    if (chunks < 0) {
-        free(buf);
-        return -1;
-    }
+    chunks = split(f, filesize, dir, max_word_length, bufsize);
 
+    size_t mem = sysconf(_SC_AVPHYS_PAGES)*sysconf(_SC_PAGESIZE);
+    max_store_elem = mem*0.7 - (filesize/bufsize)*max_word_length;
+
+    cerr << "Start merge " << chunks << " chunks with max store buffer =" << max_store_elem/pow(10,9) << "GB" << endl;
     // Phase 2: merge chunks.
-    chunk_offset = 0;
-
     multiset<pair<ifstream*, string>, lessKMerge> k_merge;//<chunk_fs,text_value>
     vector<ifstream*> chunk_fs_ptrs;
     for(int i=0; i<chunks; i++){
-        ifstream* fs = new ifstream(form_filename(i, dir));
+        ifstream* fs = new ifstream(form_filename(i, dir), ifstream::binary);
         string word;
         (*fs) >> word;
         chunk_fs_ptrs.push_back(fs);
         pair<ifstream*, string> value(fs,word);
         k_merge.insert(value);
-//        cout << "value " << i << ":" << value.second << endl;
     }
     int count=0;
+    int size_of_text = 0;
+    vector<string> data;
     while(!k_merge.empty()){
-//        printf("Sorted: %s\n",k_merge.begin()->second);
-        cout << k_merge.begin()->second << endl;
+        if(size_of_text +k_merge.begin()->second.size() < max_store_elem){
+            data.push_back(k_merge.begin()->second);
+            size_of_text += k_merge.begin()->second.size();
+        }
+        else{
+            cerr << "Push out " << size_of_text/pow(pow(2,10),3) << "GB as ";
+            copy(data.begin(), data.end(), ostream_iterator<string>(cout, "\n"));
+            cerr << "sorted " << count << " words" << endl;
+            data.clear();
+            data.push_back(k_merge.begin()->second);
+            size_of_text = k_merge.begin()->second.size();
+        }
         count++;
         ifstream* fs = k_merge.begin()->first;
-        string word;
         if(fs->eof()){
+            cerr << "Remove a chunk out of data! ==>" << k_merge.size() << " chunks remain data!";
             k_merge.erase(k_merge.begin());
             delete fs;
             continue;
         }
+        string word;
         (*fs) >> word;
         k_merge.erase(k_merge.begin());
         pair<ifstream*, string> temp(fs, word);
         k_merge.insert(temp);
     }
-//    cout << "Total: " << count << endl;
-
-//    while (chunk_offset < bufsize)
-//    {
-//        fprintf(stderr, "-> Merging chunks at offset %zu/%zu\n", chunk_offset, bufsize);
-//        merge(dir, buf, bufsize, chunks, chunk_offset);
-//        chunk_offset += bufsize / chunks;
-//    }
-
-//    free(buf);
     return 0;
 }
 
 int main(int argc, const char *argv[])
 {
-//    FILE *f;
-
-//    cout << "physic pages = " << sysconf(_SC_PHYS_PAGES) << endl
-//         << "page size = " << sysconf(_SC_PAGE_SIZE) << endl
-//         << "size = " << sysconf(_SC_PHYS_PAGES)*sysconf(_SC_PAGE_SIZE) << endl;
-
+    size_t mem = sysconf(_SC_AVPHYS_PAGES)*sysconf(_SC_PAGESIZE);
     ifstream *f;
     char *dirpath;
     size_t bufsize;
-    char dirname[] = "sort.XXXXXX";
+    char dirname[] = "/media/nguyennd/win/test/tmp.XXXXXX";
     struct stat sb;
     off_t file_size;
     clock_t start, end;
@@ -279,16 +221,43 @@ int main(int argc, const char *argv[])
     }
 
     errno = 0;
-    // Check that buffer is aliquot part of file size.
     if (stat(argv[1], &sb)) {
         perror("stat");
         exit(EXIT_FAILURE);
     }
 
-    file_size = sb.st_size;
-    bufsize = file_size/16;
 
-//    f = fopen(argv[1], "rb");
+    file_size = sb.st_size;
+
+    cerr << "File size = " << file_size/pow(pow(2,10),3) << "GB,  mem = " << mem/pow(pow(2,10),3) << "GB" << endl;
+
+    if(file_size < (off_t)mem/3){
+        start = clock();
+        ifstream file(argv[1]);
+        std::vector<string> data;
+        cerr << "User iternal sort:" << endl;
+        cerr << "Start reading file!" << endl;
+        while(!file.eof()){
+            string word;
+            file >> word;
+            data.push_back(word);
+        }
+        cerr << "Finish reading file!" << endl;
+        std::make_heap(data.begin(), data.end());
+        std::sort_heap(data.begin(), data.end());
+        cerr << "Finish sorting data!" << endl;
+        copy(data.begin(), data.end(), ostream_iterator<string>(cout, "\n"));
+        end = clock();
+        cerr << file_size/pow(pow(2,10),3) << "GB sorted in " << (double)(end-start)/CLOCKS_PER_SEC << endl;
+        return 0;
+    }
+
+
+
+    bufsize = mem/8;
+    std::cerr << "Buffer size=" << bufsize/pow(pow(2,10),3) << "GB,  mem available=" << mem/pow(pow(2,10),3) << "GB" <<endl;
+    cerr << "Approximately use " << file_size/bufsize << " chunks" << endl;
+
     f = new ifstream(argv[1]);
     if (f == NULL) {
         perror("fopen");
@@ -309,11 +278,8 @@ int main(int argc, const char *argv[])
         goto err;
     }
     end = clock();
-    fprintf(stderr, "%ld bytes sorted in %f seconds\n", file_size, (double)(end - start) / CLOCKS_PER_SEC);
+    cerr << file_size/pow(pow(2,10),3) << "GB sorted in " << (double)(end-start)/CLOCKS_PER_SEC << endl;
 
 err:
-
-//    fclose(f);
-    f->close();
     return 0;
 }
